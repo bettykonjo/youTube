52facb9aac0ee688348de0bcb53ef075b2a822c9
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-03-26 08:49:49 -0400

    working on functions

--- codevolution/javascriptBeginners/js/script.js
+++ codevolution/javascriptBeginners/js/script.js
@@ -1,40 +1,40 @@
-// conditional Statements if/else
-const num = -8
-if (num > 0) {
-console.log('Number is posetive')
-} else {
-    console.log ('number is not posetive')
-}
-// if/else if/else
+// // conditional Statements if/else
+// const num = -8
+// if (num > 0) {
+// console.log('Number is posetive')
+// } else {
+//     console.log ('number is not posetive')
+// }
+// // if/else if/else
 
-const numb = 6
-if (numb > 0) {
-console.log('Number is posetive')
-} else if(numb < 0){
-    console.log('number is negative')
-} else {
-    console.log('numberrs is 0')
-}
+// const numb = 6
+// if (numb > 0) {
+// console.log('Number is posetive')
+// } else if(numb < 0){
+//     console.log('number is negative')
+// } else {
+//     console.log('numberrs is 0')
+// }
 
-// switch
-const color =red
-switch (color){
-    case 'red':
-        console.log('color is red')
-        break
-     case 'blue':
-         console.log('color is blue')
-         break
-     case 'gold':
-         console.log('color is gold')  
-         break
-    case 'green':
-        console.log('color is green')
-        break
-        default:
-            console.log('not a valid color')
-}
-// looping code
+// // switch
+// const color =red
+// switch (color){
+//     case 'red':
+//         console.log('color is red')
+//         break
+//      case 'blue':
+//          console.log('color is blue')
+//          break
+//      case 'gold':
+//          console.log('color is gold')  
+//          break
+//     case 'green':
+//         console.log('color is green')
+//         break
+//         default:
+//             console.log('not a valid color')
+// }
+// // looping code
 // loop use to repeat a block of code
 // foor loop
 // for(initializer; Condition; final-expretion){
@@ -70,3 +70,28 @@ switch (color){
 // for (const num of numarray){
 //     console.log('interation number is ' + num)
 // }
+// // functions
+// function name (paramerts1, parametir2, parametir3){
+//     //code to be executed
+// }
+// function greet(userName){
+//     console,log ('Good morining '+ userName)
+// }
+// greet('betty')
+// greet('marru')
+// greet('amene')
+// function add(a,b){
+// return a+b
+// }
+// const sum = add(8,5)
+// console.log(sum)
+// console.log(add(9,14))
+
+// const arrwsum = (a,b) =>{
+//  return a+b
+// }
+// console.log(arrwsum(40,50))
+
+// const arrowsums =(a,b) =>a+b
+// console.log(arrowsums(65,2))
+// const addfive  =sum => sum + 5
