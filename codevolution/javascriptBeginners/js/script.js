af0f8170e52f97b21bdcbf4a69224ee8a1e4348d
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-03-26 08:11:11 -0400

    practicing loopimg

--- codevolution/javascriptBeginners/js/script.js
+++ codevolution/javascriptBeginners/js/script.js
@@ -34,3 +34,39 @@ switch (color){
         default:
             console.log('not a valid color')
 }
+// looping code
+// loop use to repeat a block of code
+// foor loop
+// for(initializer; Condition; final-expretion){
+//     // the code we want to return
+// }
+// for( let i =1; i<=5; i++){
+//     console.log('interation number is ' + i)
+// }
+// whille loop
+// initializer
+//  while(Condition){
+//     // code to return
+//     final-expression
+// }
+// let i = 1
+// while ( i <= 5){
+//     console.log('interation number is ' + i)
+//     i++
+// }
+
+// // do will loop
+// let i = 2
+// do{
+//     console.log('interation number is ' + i) 
+//     i++
+// } while ( i <= 5)
+
+// // foor ..of loop
+// for (const item of array){
+//     // code to run
+// }
+// const numarray = [1,2,3,4,5]
+// for (const num of numarray){
+//     console.log('interation number is ' + num)
+// }
