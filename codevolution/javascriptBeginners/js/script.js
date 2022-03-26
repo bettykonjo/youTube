29781cf29a44f01d6a0f7980ff70f050c96c4eaa
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-03-26 07:22:39 -0400

    practicing conditional statements

---
+++ codevolution/javascriptBeginners/js/script.js
@@ -0,0 +1,36 @@
+// conditional Statements if/else
+const num = -8
+if (num > 0) {
+console.log('Number is posetive')
+} else {
+    console.log ('number is not posetive')
+}
+// if/else if/else
+
+const numb = 6
+if (numb > 0) {
+console.log('Number is posetive')
+} else if(numb < 0){
+    console.log('number is negative')
+} else {
+    console.log('numberrs is 0')
+}
+
+// switch
+const color =red
+switch (color){
+    case 'red':
+        console.log('color is red')
+        break
+     case 'blue':
+         console.log('color is blue')
+         break
+     case 'gold':
+         console.log('color is gold')  
+         break
+    case 'green':
+        console.log('color is green')
+        break
+        default:
+            console.log('not a valid color')
+}
